033b13a298522bb84147b3f32e6e406ecf1d2c2e
DCM <dcm@cs.uns.edu.ar>
2025-11-16 15:40:50 -0300

    modif

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,15 @@ function App() {
 
   return (
     <>
+      <h1>Mi Contador</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>
+      <p>
+        TADW 2025
+      </p>
     </>
   )
 }
